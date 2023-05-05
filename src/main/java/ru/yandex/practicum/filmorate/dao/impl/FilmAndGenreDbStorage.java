fbdf98b168072674c4938088bb7472f087d221d8
EugeneStartsev <123lol123lolik@gmail.com>
2023-05-05 16:10:06 +0300

    feat: Добавить FilmAndGenreStorage, FriendshipStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmAndGenreDbStorage.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import ru.yandex.practicum.filmorate.dao.FilmAndGenreStorage;
+
+public class FilmAndGenreDbStorage implements FilmAndGenreStorage {
+
+}
